bd515370d99a89c5f21dae4285b44c2484f35d54
senri <yhasegawa0307@gmail.com>
2021-12-31 02:39:30 +0900

    class change to className

--- components/Layout.js
+++ components/Layout.js
@@ -30,7 +30,7 @@ const Layout = (props) => {
       <hr></hr>
 
       <tw>
-        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
       </tw>
 
       <footer>
